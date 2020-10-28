5df3ae519cfcc05435dd03a7a2c50b11be471f2f
bruno-wolf <bruno.m.lobato@gmail.com>
2020-10-28 19:04:01 +0000

    feat: added create repository route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -16,7 +15,19 @@ app.get('/repositories', (request, response) => {
 });
 
 app.post('/repositories', (request, response) => {
-	// TODO
+	const { title, url, techs } = request.body;
+
+	const newRepository = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0
+	};
+
+	repositories.push(newRepository);
+
+	return response.json(newRepository);
 });
 
 app.put('/repositories/:id', (request, response) => {
